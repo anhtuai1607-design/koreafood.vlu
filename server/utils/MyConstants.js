e9245f7561ef6fad51db8e339a247d3b6c9d7645
anhtuai1607-design <anhtuai16.07@gmail.com>
2026-03-31 19:18:36 +0700

    server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,14 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.y4k0cem.mongodb.net',
+  DB_USER: 'nhom2',
+  DB_PASS: '123',
+  DB_DATABASE: 'koreafood',
+
+  EMAIL_USER: 'anhtuai16.07@gmail.com',
+  EMAIL_PASS: 'edvwsuwfojaolxf',
+
+  JWT_SECRET: 'abdef',
+  JWT_EXPIRES: '3287246589523977687' 
+};
+
+module.exports = MyConstants;
